165ac9d9aa608db7a48c2dda0e4ed5aea28f565c
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-19 11:05:01 -0600

    added bootstrap styling using Container component to group and size rows

--- money/src/StocksPage.js
+++ money/src/StocksPage.js
@@ -5,25 +5,39 @@ import Filter from "./Filter";
 import SearchBar from "./SearchBar";
 import Sort from "./Sort";
 import Watchlist from "./Watchlist";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function StocksPage() {
   return (
     console.log("this is the Stocks main page"),
     (
-      <div>
-        <h1>NYSE Stock Exchange</h1>
-        <div>
-          <SearchBar />
-          <Sort />
-          <Filter />
-        </div>
-        <div>
-          <CardContainer />
-        </div>
-        <div>
-          <Watchlist />
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <h1>NYSE Stock Exchange</h1>
+        </Row>
+        <Row xs="auto">
+          <Col>
+            <SearchBar />
+          </Col>
+          <Col>
+            <Sort />
+          </Col>
+          <Col>
+            <Filter />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={8}>
+            <CardContainer />
+          </Col>
+
+          <Col md={4}>
+            <Watchlist />
+          </Col>
+        </Row>
+      </Container>
     )
   );
 }
