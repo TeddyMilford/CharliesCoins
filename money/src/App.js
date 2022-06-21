5615ed8de6fa809166099dee759e8ee34ec83822
Teddy Milford <teddy.milford@gmail.com>
2022-06-21 12:43:54 -0600

    card colors

--- money/src/App.js
+++ money/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import CoinsPage from "./CoinsPage";
 import LearnPage from "./LearnPage";
@@ -7,12 +7,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Watchlist from "./Watchlist";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coincap.io/v2/assets")
+      .then((response) => response.json())
+      .then((data) => {
+        setCoins(data.data);
+      });
+  }, []);
+
   return (
     <div>
       <NavBar />
       <Switch>
         <Route path="/discover">
-          <CoinsPage></CoinsPage>
+          <CoinsPage coinData={coins}></CoinsPage>
         </Route>
         <Route path="/watchlist">
           <Watchlist></Watchlist>
--- money/src/TickerCards.js
+++ money/src/TickerCards.js
@@ -2,16 +2,28 @@
 
 import Card from "react-bootstrap/Card";
 
-function TickerCards() {
-  return (
-    <Card>
-      <Card.Header>$ TICKER</Card.Header>
-      <Card.Body>
-        <Card.Title> Company Name </Card.Title>
-        <Card.Text>Price Details</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+function TickerCards({ coin, index }) {
+  if (coin !== undefined) {
+    console.log(coin.changePercent24Hr);
+    let color;
+    let split = coin.changePercent24Hr.split("");
+
+    let deter = split.includes("-") ? (color = false) : (color = true);
+
+    return (
+      <Card bg={color ? "success" : "danger"}>
+        <Card.Header>{coin.symbol}</Card.Header>
+        <Card.Body>
+          <Card.Title>{coin.name} </Card.Title>
+          <Card.Text>{coin.priceUsd}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  } else {
+    return null;
+  }
 }
 
 export default TickerCards;
+
+// bg={color ? "danger" : "success"}
